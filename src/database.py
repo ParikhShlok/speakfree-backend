0b3af55254e30a95805a6a8772ac74e1393bfbdf
Parikh Shlok <146865809+ParikhShlok@users.noreply.github.com>
2025-12-25 15:41:55 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -4,8 +4,16 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL not set")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
