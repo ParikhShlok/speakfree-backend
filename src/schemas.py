c97a8cae9c534f9314e06238309d1961dc3767c2
Parikh Shlok <146865809+ParikhShlok@users.noreply.github.com>
2025-12-25 20:22:14 +0530

    Delete src/schemas.py

--- src/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class SignupRequest(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-
-class SignupResponse(BaseModel):
-    message: str
